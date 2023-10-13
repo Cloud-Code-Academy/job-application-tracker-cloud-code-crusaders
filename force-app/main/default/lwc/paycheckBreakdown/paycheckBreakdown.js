f3ba62932c014cd8ec4a84235416c57ed194a5f4
Theron Troxel <therontroxel@yahoo.com>
2023-10-13 13:18:37 -0500

    PaycheckBreakdown LWC 1st draft

--- force-app/main/default/lwc/paycheckBreakdown/paycheckBreakdown.js
+++ force-app/main/default/lwc/paycheckBreakdown/paycheckBreakdown.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class PaycheckBreakdown extends LightningElement {}
+export default class PaycheckBreakdown extends LightningElement {
+    yearlyPay = 0;
+    sixMonthsPay = 0;
+    monthlyPay = 0;
+    biWeeklyPay = 0
+    weeklyPay = 0;
+    
+    calculatePay(event) {
+        this.yearlyPay = parseFloat(event.target.value);
+
+        // Calculate other pay values here
+        this.sixMonthsPay = (parseFloat(this.yearlyPay) / 2).toLocaleString(undefined, { maximumFractionDigits: 2 });
+        this.monthlyPay = (parseFloat(this.yearlyPay) / 12).toLocaleString(undefined, { maximumFractionDigits: 2 });
+        this.biWeeklyPay = (parseFloat(this.yearlyPay) / 26).toLocaleString(undefined, { maximumFractionDigits: 2 });
+        this.weeklyPay = (parseFloat(this.yearlyPay) / 52).toLocaleString(undefined, { maximumFractionDigits: 2 });
+    }
+}
