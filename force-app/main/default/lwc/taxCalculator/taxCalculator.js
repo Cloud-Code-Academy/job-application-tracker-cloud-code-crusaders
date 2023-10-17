66271d3f7c0578ad2522e5ad2c916d335f068403
knuckles4ever <115323225+knuckles4ever@users.noreply.github.com>
2023-10-16 19:01:02 -0500

    Update all calculations

--- force-app/main/default/lwc/taxCalculator/taxCalculator.js
+++ force-app/main/default/lwc/taxCalculator/taxCalculator.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,} from 'lwc';
 const socialSecurityRate = .0620;
 const medicareWithholdingRate = .0145;
 const federalTaxRates = [
@@ -18,25 +18,52 @@ export default class TaxCalculator extends LightningElement {
     sixMonthsPay = 0;
     monthlyPay = 0;
     biWeeklyPay = 0;
+    federalWithholdings = 0;
+    socialSecurityWithholdings = 0;
+    medicareWitholdings = 0;
    
-    calculatePay(event) {
-        this.salary = parseFloat(event.target.value);
+
+        calculatePay(event) {
+            const inputValue = event.target.value.trim(); // Remove leading/trailing spaces
+        
+            if (inputValue === '') {
+                // If the input is empty, reset all values to zero
+                this.salary = 0;
+                this.yearlyPay = 0;
+                this.yearlyRoundedPay = 0;
+                this.sixMonthsPay = 0;
+                this.monthlyPay = 0;
+                this.biWeeklyPay = 0;
+                this.federalWithholdings = 0;
+                this.socialSecurityWithholdings = 0;
+                this.medicareWitholdings = 0;
+            } else {
+                // If the input is not empty, proceed with calculations
+                this.salary = parseFloat(inputValue);
     
         // For calculating Federal Tax, you can loop through the tax brackets
         for (const bracket of federalTaxRates) {
             if (this.salary >= bracket.minIncome && this.salary <= bracket.maxIncome) {
                 const federalTax = (bracket.rate * this.salary) - (bracket.minIncome * bracket.rate);
- 
+                this.federalWithholdings = federalTax.toLocaleString(undefined, { maximumFractionDigits: 2 });
                 this.yearlyPay = (this.salary - (federalTax + (this.salary * socialSecurityRate) + (this.salary * medicareWithholdingRate)));
                 break;
             }
         }
-    
+       // Calculate Social Security Tax and Medicare Tax if salary is greater than zero
+       if (this.salary > 0) {
+        this.socialSecurityWithholdings = (this.salary * socialSecurityRate).toFixed(2); 
+        this.medicareWitholdings = (this.salary * medicareWithholdingRate).toFixed(2); 
+    } else {
+        this.socialSecurityWithholdings = 0;
+        this.medicareWitholdings = 0;
+    }
          // Calculate other pay values here
+    
          this.yearlyRoundedPay = (parseFloat(this.yearlyPay)).toLocaleString(undefined, { maximumFractionDigits: 2 });
          this.sixMonthsPay = (parseFloat(this.yearlyPay) / 2).toLocaleString(undefined, { maximumFractionDigits: 2 });
          this.monthlyPay = (parseFloat(this.yearlyPay) / 12).toLocaleString(undefined, { maximumFractionDigits: 2 });
          this.biWeeklyPay = (parseFloat(this.yearlyPay) / 26).toLocaleString(undefined, { maximumFractionDigits: 2 });
-     }
-}
-  
+       }
+    }
+} 
