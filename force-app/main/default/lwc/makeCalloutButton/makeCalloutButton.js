3a7ed162b8003192900dcf6e8f0bfbc83ee4c238
thehilla <110313763+thehilla@users.noreply.github.com>
2023-10-22 22:35:58 -0500

    still working on lwc for callout

--- force-app/main/default/lwc/makeCalloutButton/makeCalloutButton.js
+++ force-app/main/default/lwc/makeCalloutButton/makeCalloutButton.js
@@ -1,19 +1,23 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import postJoobleCallout from '@salesforce/apex/JoobleCallout.postJoobleCallout';
 
-export default class JoobleCallout extends LightningElement {
 
-    @track MakeCalloutButton;
+/*export default class MakeCalloutButton extends LightningElement {
 
-    makeCallout() {
+   @wire (postJoobleCallout) newJobs;
 
+        
+   
        const calloutURL = 'https://jooble.org/api/2a3be2fb-3555-410e-924b-8e2c964de9d3';
         fetch(calloutURL, {
-            method: "POST"
-        }).then((response) => response.json())
-            .then(repos => {
-                console.log(repos)
-              //  this.MakeCalloutButton = repos.;
-                console.log(this.MakeCalloutButton);
+            method: "POST"})
+            .then(response => {
+            console.log(response);
+              if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response);
+            }
             });
     }
 }
